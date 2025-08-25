244678a52d7776913e128818b21753eedc979daf
BalajiHariharasudhan <bala3507563@gmail.com>
2025-08-25 22:46:47 +0530

    Solution for Sqrt(x)

---
+++ Sqrt(x).java
@@ -0,0 +1,8 @@
+leetcode problem:Sqrt(x)
+leetcode id:https://leetcode.com/problems/sqrtx/
+class Solution {
+    public int mySqrt(int x) {
+        int res=(int)Math.sqrt(x);
+        return res;
+    }
+}
