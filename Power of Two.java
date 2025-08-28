78354f42e7fb2d577c229c1e1f5ffd33f125c992
BalajiHariharasudhan <bala3507563@gmail.com>
2025-08-28 21:09:30 +0530

    Solution for Power of Two

---
+++ Power of Two.java
@@ -0,0 +1,9 @@
+/*
+Leetcode:Power of Two
+Leetcode id:https://leetcode.com/problems/power-of-two
+*/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n>0 && (n & (n-1))==0;
+    }
+}
