5ea8d3c7925627ccbf72429c12ab635d9bc3b434
BalajiHariharasudhan <bala3507563@gmail.com>
2025-09-06 19:56:13 +0530

    Solution for  Matrix Diagonal Sum

---
+++ Matrix Diagonal Sum.java
@@ -0,0 +1,18 @@
+/*
+Leetcode:Matrix Diagonal Sum
+Leetcode Id:https://leetcode.com/problems/matrix-diagonal-sum/
+*/
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int row=mat.length;
+        int sum=0;
+        for(int i=0;i<row;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][row-1-i];
+        }
+        if(row%2!=0){
+            sum-=mat[row/2][row/2];
+        }
+        return sum;
+    }
+}
