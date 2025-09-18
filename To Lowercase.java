b78a869da2b10aaa4e827a7d7c66240daee95455
BalajiHariharasudhan <bala3507563@gmail.com>
2025-09-18 21:23:57 +0530

    Solution for To Lowercase

---
+++ To Lowercase.java
@@ -0,0 +1,10 @@
+/*
+Leetcode:To Lowercase
+Leetcode Id:https://leetcode.com/problems/to-lower-case/
+*/
+class Solution {
+    public String toLowerCase(String s) {
+        String str=s.toLowerCase();
+        return str;
+    }
+}
