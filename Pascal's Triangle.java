8565ccd5ea692d77f9c1d0b06bf64a7539b3ddb6
BalajiHariharasudhan <bala3507563@gmail.com>
2025-09-22 18:35:43 +0530

    Solution for Pascal's Triangle

---
+++ Pascal's Triangle.java
@@ -0,0 +1,23 @@
+/*
+Leetcode:Pascal's Triangle
+Leetcode Id:https://leetcode.com/problems/pascals-triangle/
+*/
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i){
+                    row.add(1);
+                }
+                else{
+                    List<Integer> pre=res.get(i-1);
+                    row.add(pre.get(j)+pre.get(j-1));
+                }
+            }
+            res.add(row);
+        }
+        return res;
+    }
+}
