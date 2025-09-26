c7fb2ec4f933a6b7052744bf76f80de1c04f9324
BalajiHariharasudhan <bala3507563@gmail.com>
2025-09-26 22:33:58 +0530

    Solution for Power of three

---
+++ Power of three.java
@@ -0,0 +1,20 @@
+/*
+Leetcode:Power of three
+Leetcode Id:https://leetcode.com/problems/power-of-three/
+*/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%3!=0){
+         return false;
+        }
+        return isPowerOfThree(n/3);
+        
+    }
+    
+}
