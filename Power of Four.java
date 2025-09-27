4313906af8bc1bf65df1f0b685034c629895c34e
BalajiHariharasudhan <bala3507563@gmail.com>
2025-09-27 11:17:04 +0530

    Solution for Power of Four

---
+++ Power of Four.java
@@ -0,0 +1,18 @@
+/*
+Leetcode:342. Power of Four
+Leetcode Id:https://leetcode.com/problems/power-of-four/
+*/
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        if(n%4!=0){
+            return false;
+        }
+        return isPowerOfFour(n/4);
+    }
+}
