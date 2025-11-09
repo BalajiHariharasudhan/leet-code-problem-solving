82b3b43d0bd3e0097f69065bfc2df15d31fab59c
BalajiHariharasudhan <bala3507563@gmail.com>
2025-11-09 20:31:21 +0530

    Add solution for smallest even multiple
    Implement method to find the smallest even multiple of a number.

---
+++ Smallest Even Multiple.java
@@ -0,0 +1,13 @@
+/*
+Leetcode: Smallest Even Multiple
+*/
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n%2!=0){
+            return n*2;
+        }
+        else{
+            return n;
+        }
+    }
+}
