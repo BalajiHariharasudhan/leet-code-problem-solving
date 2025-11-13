3ee6063512673d35d1673185a2b1ec94d976a108
BalajiHariharasudhan <bala3507563@gmail.com>
2025-11-13 21:14:49 +0530

    Implement solution for 1-bit and 2-bit characters

---
+++ 1-bit and 2-bit Characters.java
@@ -0,0 +1,16 @@
+/*
+Leetcode:1-bit and 2-bit Characters
+*/
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            if (bits[i] == 0) {
+                i++;
+            } else {
+                i += 2;
+            }
+        }
+        return i == bits.length - 1;
+    }
+}
