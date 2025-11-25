53c0b9caef919a4c8209e1557ef9a23449473b48
BalajiHariharasudhan <bala3507563@gmail.com>
2025-11-25 17:24:37 +0530

    Add stableMountains method in Solution class
    Implement stableMountains method to find indices of stable mountains above a threshold.

---
+++ Find Indices of Stable Mountains.java
@@ -0,0 +1,14 @@
+/*
+Leetcode: Find Indices of Stable Mountains
+*/
+class Solution {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        ArrayList<Integer> res=new ArrayList<>();
+        for(int i=1;i<height.length;i++){
+            if(height[i-1]>threshold){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
