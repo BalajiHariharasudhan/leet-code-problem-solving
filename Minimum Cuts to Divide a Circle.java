b6e8f59ea87ece2a780efe2d04cccfa1c1c6b4ea
BalajiHariharasudhan <bala3507563@gmail.com>
2025-11-26 18:31:09 +0530

    Implement solution for minimum cuts to divide a circle

---
+++ Minimum Cuts to Divide a Circle.java
@@ -0,0 +1,14 @@
+/*
+Leetcode:Minimum Cuts to Divide a Circle
+*/
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n==1){
+            return 0;
+        }
+        else if(n%2==0){
+            return n/2;
+        }
+        return n;
+    }
+}
