c3bc6dda0a9bdd1e9611de02aef136ee9a96e779
BalajiHariharasudhan <bala3507563@gmail.com>
2025-12-14 18:05:52 +0530

    Add solution for removing duplicates from sorted array
    Implement method to remove duplicates allowing at most two occurrences.

---
+++ Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,20 @@
+/*
+Leetcode: Remove Duplicates from Sorted Array II
+*/
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index=1;
+        int occurance=1;
+        for (int i=1;i<nums.length;i++) {
+            if (nums[i]==nums[i-1]){
+                occurance += 1;
+            }else{
+                occurance = 1;
+            }
+            if (occurance<= 2){ 
+                nums[index++]=nums[i];
+            }
+        }
+        return index;
+    }
+}
